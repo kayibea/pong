535f67f65e1ff5e08a35d252a22ca247e27eb910
kayibea <gatse1531@gmail.com>
2024-11-09 20:34:53 +0100

    ♻️ refactor code

--- src/entities/Entity.ts
+++ src/entities/Entity.ts
@@ -1,26 +1,26 @@
 import Vec2 from 'utils/Vector2';
 
 export default abstract class Entity {
-  protected _size: [number, number];
+  protected _size: readonly [number, number];
   protected _position: Vec2;
   protected _velocity: Vec2;
 
-  constructor(size: [number, number], pos = Vec2.zero, velocity = Vec2.zero) {
+  constructor(size: [number, number], pos = new Vec2(), velocity = new Vec2()) {
     this._size = size;
     this._position = pos;
     this._velocity = velocity;
   }
 
   public get size(): [number, number] {
-    return this._size;
+    return [...this._size];
   }
 
   public get width(): number {
-    return this.size.at(0) || 0;
+    return this._size[0];
   }
 
   public get height(): number {
-    return this.size.at(1) || 0;
+    return this._size[1];
   }
 
   public get position(): Vec2 {
@@ -32,12 +32,10 @@ export default abstract class Entity {
   }
 
   public set position(pos: Vec2) {
-    this.position.x = pos.x;
-    this.position.y = pos.y;
+    this._position = new Vec2(pos.x, pos.y);
   }
 
   public set velocity(vel: Vec2) {
-    this._velocity.x = vel.x;
-    this._velocity.y = vel.y;
+    this._velocity = new Vec2(vel.x, vel.y);
   }
 }
